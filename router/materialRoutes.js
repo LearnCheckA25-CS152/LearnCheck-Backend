ee3809edcf863cefc336a039d7f5fb0dec74d520
mnbvcxzz4869 <94119615+mnbvcxzz4869@users.noreply.github.com>
2025-11-06 14:41:17 +0700

    rename material router

--- router/materialRoutes.js
+++ router/materialRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import {getMaterialById} from "../controller/materialController.js";
 
-const router = express.Router();
+const materialRouter = express.Router();
 
-router.get('/tutorials/:tutorialId', getMaterialById);
+materialRouter.get('/tutorials/:tutorialId', getMaterialById);
 
-export default router;
+export default materialRouter;
